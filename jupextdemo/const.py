b0c5a43df3100ee1eccee7f6bf00807dffe916f7
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-02-27 16:14:38 +0530

    updating

--- jupextdemo/const.py
+++ jupextdemo/const.py
@@ -7,7 +7,7 @@ RG_PLACEHOLDER = 'resource_name_place_holder'
 
 PORT_NUMBER_DEFAULT = '8080'
 APP_NAME_DEFAULT = 'k8sdemo'
-RELEASE_NAME = 'aksappupdemo'
+RELEASE_NAME = 'aksappupdemo2'
 
 # Checkin message strings
 
